020c77c92a93caae565fb81905694ccd9acfb895
sabuk9999 <sabaokradze2009@gmail.com>
2024-06-14 17:08:29 +0400

    commit

--- level 4/homework.py
+++ level 4/homework.py
@@ -1,8 +1,13 @@
 from turtle import*
-width(7)
+bgcolor("skyblue")
+#we want paint a house
 
 width(7)
 color("purple")
+penup()
+goto(-200, -200)
+pendown()
+
 begin_fill()
 
 
@@ -29,12 +34,15 @@ right(90)
 forward(120)
 end_fill()
 penup()
-goto(200, 200)
+goto(0, 0)
 pendown()
 
+#paint the roof
 
 
 color("red")
+
+
 begin_fill()
 right(150)
 forward(200)
@@ -44,7 +52,7 @@ end_fill()
 
 
 penup()
-goto(50, 110)
+goto(-150, -60)
 pendown()
 
 
@@ -69,7 +77,7 @@ right(90)
 
 
 penup()
-goto(150, 110)
+goto(-50, -60)
 pendown()
 right(90)
 forward(40)
@@ -86,39 +94,94 @@ forward(40)
 right(90)
 end_fill()
 
-
+width(3)
 penup()
-goto(10, 110)
-color("purple")
+forward(150)
+pendown()
+color("black")
+begin_fill()
+forward(75)
+right(90)
+forward(75)
+right(90)
+forward(40)
+right(90)
+forward(75)
+end_fill()
+#The GOA text on the flag
+penup()
+goto(-50, 125)
+color("green")
+write("GOA", font=("Arial", 26, "bold"))
+
+goto(-770, -200)
+color("lightgreen")
+begin_fill()
+right(180)
+pendown()
+forward(1540)
+
 right(90)
 forward(200)
+right(90)
+forward(1540)
+right(90)
+forward(200)
+end_fill()
+#draw trunk
+penup()
+goto(-600, -200)
 pendown()
+color("brown")
+begin_fill()
+setheading(90)
+forward(150)
+right(90)
+forward(20)
+right(90)
+forward(150)
+end_fill()
 
-forward(90)
-left(200)
-
-left(90)
-forward(110)
-
-
-right(110)
-forward(110)
-
-
-
-
-
-
-
-
-
-
+#Draw leaves
+penup()
+goto(-690, 50)
+pendown()
+color("green")
+begin_fill()
+circle(100)
+end_fill()
 
 
+#Draw trunk
+penup()
+goto(600, -200)
+pendown()
+color("brown")
+begin_fill()
+setheading(90)
+forward(150)
+right(90)
+forward(20)
+right(90)
+forward(150)
+end_fill()
+
+#Draw leaves
+penup()
+goto(510, 50)
+pendown()
+color("green")
+begin_fill()
+circle(100)
+end_fill()
 
 
 
 
+
+
+
+exitonclick()
 
 
 
@@ -218,3 +281,91 @@ exitonclick()
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+exitonclick
